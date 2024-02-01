31d59a713f2847b2bbf345f31009c1d49fbf732b
Bharat Bhooshan <71065319+B8219@users.noreply.github.com>
2024-02-01 08:22:43 +0530

    Delete dice_roller.py

--- dice_roller.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-ques = input('Do you want to dice Y/N : ')
-
-while ques.upper() == 'Y':
-    print(f'your number is {randint(1,6)}')
-    ques = input('Do you want to roll again !!')
-else:
-    print('Thank you For Playing')
